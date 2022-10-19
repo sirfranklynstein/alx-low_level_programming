07f08b23cd35ef145b2696a6ac12d45a1089174f
sirfranklynstein <113608312+sirfranklynstein@users.noreply.github.com>
2022-10-19 13:25:54 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
-* main- Entry point
-*
-* Return: 0 (Success)
-*/
+ * main- Entry point
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
-  {
-  int i;
-  char p[] = "_putchar";
-  
-  for (i = 0; i < 8; i++)
-  {
-     _putchar(p[i]);
-  }
-  _putchar('\n');
-  return (0);
-  }
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
