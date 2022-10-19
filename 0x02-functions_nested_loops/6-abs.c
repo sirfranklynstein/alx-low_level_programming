19363f64b063c752d9d8746b743ed71b49d99032
sirfranklynstein <113608312+sirfranklynstein@users.noreply.github.com>
2022-10-19 15:47:53 +0100

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - return an absolute value of an integer
+ *
+ * @n: convert number to absolute number
+ *
+ * Return: An absolute value of int
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
