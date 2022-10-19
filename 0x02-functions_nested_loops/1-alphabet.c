d523c6643ba65d8da1b4358bcdd0c3265a3cc00b
franklyn <frankeric95@gmail.com>
2022-10-19 06:27:40 -0700

    prints a-z in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * print_alphabet - function writes a-z in lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+  int f;
+  for (f = 'a'; f <= 'z'; f++)
+    {
+      _putchar(f)
+	}
+  _putchar('\n');
+}
